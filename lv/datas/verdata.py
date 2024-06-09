395b6e901623a67f92a117573a2508c47e7f7900
Mesorah <gabrielcambara123@gmail.com>
2024-06-09 17:52:58 -0300

    att

--- lv/datas/verdata.py
+++ lv/datas/verdata.py
@@ -6,6 +6,6 @@ ARQUIVO_BRUTO = Path(__file__).parent.parent
 NOME_BANCO = 'gerando_chave_primária\\brasil.db'
 ARQUIVO_COMPLETO = ARQUIVO_BRUTO / NOME_BANCO
 
-with sqlite3.connect(ARQUIVO_COMPLETO) as connection:
+with sqlite3.connect(ARQUIVO_COMPLETO, detect_types=sqlite3.PARSE_DECLTYPES) as connection:
     for feriado in connection.execute('SELECT * FROM feriados'):
         print(feriado)
